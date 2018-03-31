cbbe4d6f7c15d824f754f3ba01548a0dd9660949
airtoxin <airtoxin@icloud.com>
2018-04-01 04:18:39 +0900

    Add name option in action

--- src/actions.js
+++ src/actions.js
@@ -5,11 +5,11 @@ export const increment = amount => createCirquitAction(state => ({
   counter: {
     count: state.counter.count + amount
   }
-}));
+}), { name: "increment" });
 
 export const reset = () => createCirquitAction(state => ({
   ...state,
   counter: {
     count: 0
   }
-}));
+}), { name: "reset" });
