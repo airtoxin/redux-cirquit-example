15fac4c9766e3ce22beaa4c1a1ec8e4f3f247c6b
airtoxin <airtoxin@icloud.com>
2018-04-18 23:22:15 +0900

    Upgrade typescript example

--- typescript/src/operations.ts
+++ typescript/src/operations.ts
@@ -1,21 +1,21 @@
-import { createCirquitAction } from "redux-cirquit";
+import {createOperation} from "redux-cirquit";
 import {State} from "./store";
 
-export const increment = (amount: number) => createCirquitAction<State>(state => ({
+export const increment = (amount: number) => createOperation<State>(state => ({
   ...state,
   counter: {
     count: state.counter.count + amount
   }
 }), { meta: { reducerName: "increment", amount } });
 
-export const reset = () => createCirquitAction<State>(state => ({
+export const reset = () => createOperation<State>(state => ({
   ...state,
   counter: {
     count: 0
   }
 }), { meta: { reducerName: "reset" } });
 
-export const setUserName = (name: string) => createCirquitAction<State>(state => ({
+export const setUserName = (name: string) => createOperation<State>(state => ({
   ...state,
   user: {
     name
