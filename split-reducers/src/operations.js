6647d01894103a1703fdbabfd8e7584f4bcdf1df
airtoxin <airtoxin@icloud.com>
2018-04-19 08:59:16 +0900

    Upgrade split-reducers example

--- split-reducers/src/operations.js
+++ split-reducers/src/operations.js
@@ -1,16 +1,16 @@
-import { createCirquitAction } from "redux-cirquit";
+import { createOperation } from "redux-cirquit";
 
-export const increment = amount => createCirquitAction(state => ({
+export const increment = amount => createOperation(state => ({
   ...state,
   count: state.count + amount
 }), { namespace: "counter", meta: { reducerName: "increment", amount } });
 
-export const reset = () => createCirquitAction(state => ({
+export const reset = () => createOperation(state => ({
   ...state,
   count: 0
 }), { namespace: "counter", meta: { reducerName: "reset" } });
 
-export const setUserName = name => createCirquitAction(state => ({
+export const setUserName = name => createOperation(state => ({
   ...state,
   name,
 }), { namespace: "user", meta: { reducerName: "setUserName", name } });
